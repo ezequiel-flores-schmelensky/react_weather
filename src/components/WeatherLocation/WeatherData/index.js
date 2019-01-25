0faa41b8782c8dfc34d0dbc7db7c143bebee3584
Ezequiel Flores <supereze.bxb@gamil.com>
2019-01-25 12:36:30 -0600

    double destructuring

--- src/components/WeatherLocation/WeatherData/index.js
+++ src/components/WeatherLocation/WeatherData/index.js
@@ -1,18 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import WeatherExtraInfo from './WeatherExtraInfo';
 import WeatherTemperature from './WeatherTemperature';
-import {
-    CLOUDY,
-} from './../../../constants/weathers';
 import './styles.css';
 
-const WeatherData = () => (
+const WeatherData = ({data:{ temperature, weatherState, humidity, wind }}) => (
     <div className="weatherDataCont">
         <WeatherTemperature 
-            temperature={20} 
-            weatherState={CLOUDY}/>
-        <WeatherExtraInfo humidity={80} wind={"10m/s"} />
+            temperature={temperature} 
+            weatherState={weatherState}/>
+        <WeatherExtraInfo humidity={humidity} wind={wind} />
     </div>
 );
+
+WeatherData.propTypes = {
+    data: PropTypes.shape({
+        temperature: PropTypes.number.isRequired,
+        weatherState: PropTypes.string.isRequired,
+        humidity: PropTypes.number.isRequired,
+        wind: PropTypes.string.isRequired,
+    }),
+}
 
 export default WeatherData;
