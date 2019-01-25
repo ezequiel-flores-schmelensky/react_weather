871edef677849cc7d603cd3a4ad2131f8d177a4b
Ezequiel Flores <supereze.bxb@gamil.com>
2019-01-25 14:44:39 -0600

    obteniendo datos desde el servidor

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -3,21 +3,19 @@ import Location from './Location';
 import WeatherData from './WeatherData';
 import {
     SUN,
-    WINDY,
 } from './../../constants/weathers';
 import './styles.css';
+
+const location = "Buenos Aires,ar";
+const api_key = "f99bbd9e4959b513e9bd0d7f7356b38d";
+const url_base_weather = "http://api.openweathermap.org/data/2.5/weather";
+
+const api_weather = `${url_base_weather}?q=${location}&appid=${api_key}`;
 
 const data = {
     temperature: 5,
     weatherState: SUN,
     humidity: 10,
-    wind: '10 m/s',
-}
-
-const data2 = {
-    temperature: 15,
-    weatherState: WINDY,
-    humidity: 20,
     wind: '10 m/s',
 }
 
@@ -31,10 +29,35 @@ class WeatherLocation extends Component {
         };
     }
 
+    getWeatherState = weather_data => {
+        return SUN;
+    }
+
+    getData = weather_data => {
+        const { humidity, temp } = weather_data.main;
+        const { speed } = weather_data.wind;
+        const weatherState = this.getWeatherState(weather_data);
+
+        const data = {
+            humidity,
+            temperature: temp,
+            weatherState,
+            wind: `${speed} m/s`,
+        }
+
+        return data;
+    }
+
     handleUpdateClick = () => {
-        console.log("Actualizado")
-        this.setState({
-            data: data2,
+        fetch(api_weather).then(resolve => {
+            return resolve.json();
+        }).then(data => {
+            const newWeather = this.getData(data);
+            console.log(newWeather);
+            debugger;
+            this.setState({
+                data: newWeather
+            });
         });
     }
 
