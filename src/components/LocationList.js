43ea701f3ddb994ecd9b7a73fdbd8345d270ff58
Ezequiel Flores <supereze.bxb@gamil.com>
2019-01-29 11:53:45 -0600

    comunicar componentes por medio de burbujeo

--- src/components/LocationList.js
+++ src/components/LocationList.js
@@ -2,12 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import WeatherLocation from './WeatherLocation' 
 
-const strToComponents = cities => (
-    cities.map(city => <WeatherLocation city={city} />)
-);
-
-const LocationList = ({cities}) => {
+const LocationList = ({cities,onSelectedLocation}) => {
     console.log(cities);
+    const handleWeatherLocationClick = city => {
+        console.log("handleWeatherLocationClick");
+        onSelectedLocation(city);
+    }
+    const strToComponents = cities => (
+        cities.map(city => 
+            (
+                <WeatherLocation 
+                key={city} 
+                city={city} 
+                onWeatherLocationClick={() => handleWeatherLocationClick(city)}/>))
+    );
     return (
     <div>
         {strToComponents(cities)}
@@ -16,6 +24,7 @@ const LocationList = ({cities}) => {
 
 LocationList.propTypes = {
     cities: PropTypes.array.isRequired,
+    onSelectedLocation: PropTypes.func,
 }
 
 export default LocationList;
