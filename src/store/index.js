502e845ed30d832815cabb9c786e8ef89c5f48c9
Ezequiel Flores <supereze.bxb@gamil.com>
2019-01-30 14:34:51 -0600

    actualizando estado de la aplicacion con cada clik

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,10 @@
 import {createStore} from 'redux';
+import {city} from './../reducers/city'
 
-export const store = createStore(() => {}, 
+const initialState = {
+    city: 'Buenos Aires,ar'
+};
+
+export const store = createStore(city, initialState, 
 window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());//Para que funcione la extensión
 
