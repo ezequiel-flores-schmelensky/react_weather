650ba71b71198dd29e686a335458316ba8761121
Ezequiel Flores <supereze.bxb@gamil.com>
2019-03-14 15:32:46 -0600

    antes de subir a heroku

---
+++ build/precache-manifest.058ba8c1765c7280e77daf1f7966ed78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9d9dd35c4d7f10e46f47",
+    "url": "/static/js/main.9d9dd35c.chunk.js"
+  },
+  {
+    "revision": "686e625f89d3247eb9b3",
+    "url": "/static/js/1.686e625f.chunk.js"
+  },
+  {
+    "revision": "9d9dd35c4d7f10e46f47",
+    "url": "/static/css/main.45086f46.chunk.css"
+  },
+  {
+    "revision": "686e625f89d3247eb9b3",
+    "url": "/static/css/1.52a65fc1.chunk.css"
+  },
+  {
+    "revision": "9958c8ebe079603298a5db5ec0680d5f",
+    "url": "/index.html"
+  }
+];
